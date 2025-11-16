188fe8be990ce1a23f61d530973aa5a40a5e00de
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-11-16 00:58:57 -0800

    apple platform setup guide fixes

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -986,6 +986,32 @@ export default function DashboardPage() {
           )}
         </div>
 
+        {/* iOS SDK Setup Guide Info Alert */}
+        <div className="info-alert">
+          <div className="info-alert-content">
+            <div className="info-alert-icon">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" fill="currentColor"/>
+              </svg>
+            </div>
+            <div className="info-alert-text">
+              <strong>Apple Platform Setup Guide</strong>
+              <span>
+                Need help setting up for iOS, macOS, or other Apple platforms? Check out our{" "}
+                <a 
+                  href="https://docs.proxlock.dev/ios-sdk/" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="info-alert-link"
+                >
+                  iOS SDK setup guide
+                </a>
+                .
+              </span>
+            </div>
+          </div>
+        </div>
+
         {/* DeviceCheck Key Section */}
         <div className="devicecheck-section">
           <div className="devicecheck-header">
@@ -993,32 +1019,6 @@ export default function DashboardPage() {
             <button className="btn-primary" onClick={() => setShowDeviceCheckModal(true)}>
               {deviceCheckKey ? "Update Key" : "+ Upload DeviceCheck Key"}
             </button>
-          </div>
-
-          {/* iOS SDK Setup Guide Info Alert */}
-          <div className="info-alert">
-            <div className="info-alert-content">
-              <div className="info-alert-icon">
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M10 2C5.58 2 2 5.58 2 10C2 14.42 5.58 18 10 18C14.42 18 18 14.42 18 10C18 5.58 14.42 2 10 2ZM10 14C9.45 14 9 13.55 9 13V9C9 8.45 9.45 8 10 8C10.55 8 11 8.45 11 9V13C11 13.55 10.55 14 10 14ZM9 6H11V4H9V6Z" fill="currentColor"/>
-                </svg>
-              </div>
-              <div className="info-alert-text">
-                <strong>Apple Platform Setup Guide</strong>
-                <span>
-                  Need help setting up for iOS, macOS, or other Apple platforms? Check out our{" "}
-                  <a 
-                    href="https://docs.proxlock.dev/ios-sdk/" 
-                    target="_blank" 
-                    rel="noopener noreferrer"
-                    className="info-alert-link"
-                  >
-                    iOS SDK setup guide
-                  </a>
-                  .
-                </span>
-              </div>
-            </div>
           </div>
 
           {!deviceCheckKey ? (
